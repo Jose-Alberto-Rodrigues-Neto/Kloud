a987421055e4888faf6902e0862d26674bd10fe1
Pedro Vinicius <pevine6508@gmail.com>
2024-07-07 20:46:13 -0300

    test: add ApplicationKtTest

---
+++ server/src/test/kotlin/project/kloud/ApplicationKtTest.kt
@@ -0,0 +1,13 @@
+package project.kloud
+
+import io.ktor.server.config.*
+import io.ktor.server.testing.*
+import kotlin.test.Test
+
+class ApplicationKtTest {
+
+    @Test
+    fun main() {
+
+    }
+}
