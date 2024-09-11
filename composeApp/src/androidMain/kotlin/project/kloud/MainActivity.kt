ef18040212ca67c1d87822aba6e695b2c86c7a82
Alberto Rodrigues <allio.professional@gmail.com>
2024-09-10 22:34:33 -0300

    feat: making navigation menu fixed at the bottom

--- composeApp/src/androidMain/kotlin/project/kloud/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/project/kloud/MainActivity.kt
@@ -1,10 +1,17 @@
 package project.kloud
 
 import App
+import Navgation.NavMenu
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
@@ -20,5 +27,20 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun AppAndroidPreview() {
-    App()
+    fun x(){
+
+    }
+    Scaffold(
+        bottomBar = {
+            NavMenu(x())
+        }
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            App()
+        }
+    }
 }
