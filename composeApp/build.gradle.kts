a27e74cf120e0f2d5731bd44a1175710b007b893
Alberto Rodrigues <allio.professional@gmail.com>
2024-09-12 17:35:46 -0300

    feat: adding navigation dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -4,12 +4,16 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsCompose)
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.5.21"
 }
 
 dependencies {
     implementation("androidx.compose.material3:material3:1.3.0")
     implementation("androidx.compose.material3:material3-window-size-class:1.3.0")
     implementation("androidx.compose.material3:material3-adaptive-navigation-suite:1.3.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+    implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.navigation.compose)
 }
 
 kotlin {
@@ -36,6 +40,7 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation("androidx.compose.material3:material3:1.3.0")
             implementation(projects.shared)
+            implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
         }
     }
 }
