a69de223ed501b9c7d1d82a80f0117a8f868d874
Alberto Rodrigues <allio.professional@gmail.com>
2024-09-16 15:49:50 -0300

    feat: adding new components to main

--- composeApp/src/androidMain/kotlin/Screens/Home.kt
+++ composeApp/src/androidMain/kotlin/Screens/Home.kt
@@ -1,11 +1,17 @@
 package Screens
 
 import Navgation.NavMenu
+import Screens.Components.BoxList
+import Screens.Components.ServiceBox
+import Screens.Components.ServiceBoxProps
+import Screens.Components.TabTitle
 import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,20 +19,33 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import project.kloud.R
+
+fun onClick(){
+
+}
+
+val boxProps = listOf(
+    ServiceBoxProps("Computer Engine", R.drawable.home, "Cloud Functions", onClick()),
+    ServiceBoxProps("Cloud Storage", R.drawable.home, "", onClick()),
+    ServiceBoxProps("Persistent Disk", R.drawable.home, "Home", onClick()),
+    ServiceBoxProps("home", R.drawable.home, "Home", onClick())
+)
 
 @Composable
 fun Home(){
-
-        Box(
+        Column(
             modifier = Modifier
-                .fillMaxSize(),
-            contentAlignment = Alignment.Center
-
+                .fillMaxSize()
         ) {
-            Text(
-            text = "Home",
-            fontSize = 65.sp,
+            TabTitle(
+                title = "Serviços",
+                icon = R.drawable.keyboard_arrow_right,
+                route = "Services",
+                onClick = onClick()
             )
+            BoxList(boxProps)
         }
 }
 
